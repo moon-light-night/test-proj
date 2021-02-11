74ed4608320964a511e59e1a3fb04eee885f4e8e
Ilya Baskakov <baskakov.11111@mail.ru>
2021-02-11 18:04:51 +0300

    ending first page

--- src/views/AddNewCard.vue
+++ src/views/AddNewCard.vue
@@ -1,9 +1,19 @@
 <template>
-  <div>
+  <div id="div">
     <div class="main-back"></div>
     <div class="nav"></div>
     <div class="emty-field __payment"></div>
-    <img class="card-position" src="@/profile/AddNewCard/images/defCard.svg" />
+    <p class="nameCard" v-if="this.cardField !== 'Master card'">
+      Название карты
+    </p>
+    <img
+      class="card-position"
+      src="@/profile/AddNewCard/images/Rectangle14.png"
+    />
+    <img class="card-wifi" src="@/profile/AddNewCard/images/wifi.svg" />
+    <img class="card-chip" src="@/profile/AddNewCard/images/chip.svg" />
+    <img class="card-stars" src="@/profile/AddNewCard/images/stars.svg" />
+    <p class="mmyy">мм / yy</p>
     <fieldset class="field">
       <legend>Название карты</legend>
       <input
@@ -24,6 +34,30 @@
       class="footer__newcard"
       src="@/profile/PersonalAccount/images/footer.png"
     />
+    <img
+      src="@/profile/AddNewCard/images/mc_vrt_rev1.svg"
+      alt="img"
+      class="logo"
+      v-if="this.cardField === 'Master card'"
+    />
+    <img
+      src="@/profile/AddNewCard/images/CB.svg"
+      alt="img"
+      class="logo-cb"
+      v-if="this.cardField === 'Master card'"
+    />
+    <img
+      src="@/profile/AddNewCard/images/UniversalBank.svg"
+      alt="img"
+      class="logo-bank"
+      v-if="this.cardField === 'Master card'"
+    />
+    <img
+      src="@/profile/AddNewCard/images/Line2.svg"
+      alt="img"
+      class="logo-line"
+      v-if="this.cardField === 'Master card'"
+    />
   </div>
 </template>
 
@@ -35,6 +69,14 @@ export default {
   }),
   validations: {
     cardField: { required },
+  },
+  mounted() {},
+  watch: {
+    cardField: function() {
+      if (this.cardField === 'Master card') {
+        console.log('working')
+      }
+    },
   },
 }
 </script>
